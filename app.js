edf0bd2215ed087ed8bdcdd39c11761d3f28c4fa
farheenfatima3 <farheenfatima961@gmail.com>
2021-08-31 03:11:12 +0530

    commented

--- app.js
+++ app.js
@@ -11,16 +11,18 @@ checkButton.addEventListener("click", dateFun)
 function dateFun(dob) {
     let inputDate = userIn.value
     let luckyNumber = getLucky.value
+    // joining the date
     inputDate = inputDate.replace(/-/g, '')
 
     let sum=0
+    // strore in variable sum so that every time i like 1,2,0.. for date 12052020 keeps on adding to each other (1+2+0+5+2+0+2+0)
     for (let i = 0; i < inputDate.length; i++) {
         sum=sum+Number(inputDate[i])
     }
-       
-        msg.innerText=sum
+    //    keep the function call
         lucky(luckyNumber,sum)
 }
+
 
 
 function lucky(luckyNumber,sum){
@@ -31,6 +33,7 @@ function lucky(luckyNumber,sum){
     msg.innerText=`Please select the date.`
     msg.style.color="#831843"
    }
+//    calculating if date which we added and user entered number have remainder 0 if yes we say its lucky
    else if(sum%luckyNumber==0){
         msg.innerText=`Congratulations!! ${userIn.value} is lucky🥳`
         msg.style.color="#064E3B"
